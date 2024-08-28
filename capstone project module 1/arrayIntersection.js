7c3cd1b12b1f8122c0057dccd97d006e30cf40fe
GauravGaware-hub <gauravgaware200@gmail.com>
2024-08-28 17:54:23 +0530

    Update arrayIntersection.js

--- capstone project module 1/arrayIntersection.js
+++ capstone project module 1/arrayIntersection.js
@@ -12,4 +12,4 @@ console.log(arrayIntersection([1, 2, 2, 3, 4, 5], [2, 3, 3, 6]));
 console.log(arrayIntersection([70, 20, 30, 50], [30, 40, 50, 60, 70])); 
 
 //Do not modify the below lines
-module.exports { calculateBMI.js }
+module.exports { arrayIntersection.js }
