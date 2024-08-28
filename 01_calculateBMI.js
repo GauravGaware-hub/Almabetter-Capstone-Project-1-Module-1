2b2b87ba9c80344a2f236004d1b1db371bcb4fc8
GauravGaware-hub <gauravgaware200@gmail.com>
2024-08-28 17:45:48 +0530

    Create 01_calculateBMI.js

---
+++ 01_calculateBMI.js
@@ -0,0 +1,49 @@
+function calculateBMI(weight, height) {
+    // Write your code here
+    const bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 || bmi < 24.9) {
+        return "Normal weight";
+    } else if (bmi >= 25 || bmi < 29.9) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
+
+// Do not modify the below lines
+module.exports = { calculateBMI };
+
+// correct code without erros 
+
+function calculateBMI(weight, height) {
+    // Write your code here
+
+    if (weight < 0 || height < 0 ){
+
+        return "invalid input!, Please enter only positive values"
+    } 
+    const bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return "Normal weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+
+
+
+}
+
+console.log(calculateBMI(52, 1.708))
+console.log(calculateBMI(-52, 1.708))
+
+// Do not modify the below lines
+module.exports = { calculateBMI };
+
